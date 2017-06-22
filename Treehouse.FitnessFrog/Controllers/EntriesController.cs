63e96b13b74d2a32fcb2f87d6e0cd0ee5e99b390
wind10 <wind10@LAPTOP-3LUOAKAG>
2017-06-22 14:50:00 +0700

    final

---
+++ Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Treehouse.FitnessFrog.Data;
+using Treehouse.FitnessFrog.Models;
+
+namespace Treehouse.FitnessFrog.Controllers
+{
+    public class EntriesController : Controller
+    {
+        private EntriesRepository _entriesRepository = null;
+
+        public EntriesController()
+        {
+            _entriesRepository = new EntriesRepository();
+        }
+
+        public ActionResult Index()
+        {
+            List<Entry> entries = _entriesRepository.GetEntries();
+
+            // Calculate the total activity.
+            double totalActivity = entries
+                .Where(e => e.Exclude == false)
+                .Sum(e => e.Duration);
+
+            // Determine the number of days that have entries.
+            int numberOfActiveDays = entries
+                .Select(e => e.Date)
+                .Distinct()
+                .Count();
+
+            ViewBag.TotalActivity = totalActivity;
+            ViewBag.AverageDailyActivity = (totalActivity / (double)numberOfActiveDays);
+
+            return View(entries);
+        }
+
+        public ActionResult Add()
+        {
+            ViewBag.SelectListItem = new SelectList(Data.Data.Activities, "Id", "Name"); //-->menu dropdown
+
+
+            return View();
+        }
+
+        //function Addsave ini dipanggil ketika lagi run di add dan di httpnya lg post;
+        //[ActionName("Add"), HttpPost]
+        //krn udah ada contructornya dan beda denngan add diatas jd bisa di hapus ActionName("Add")nya;
+        [HttpPost]
+        public ActionResult Add(Entry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                _entriesRepository.AddEntry(entry);
+
+                return RedirectToAction("Index");
+            }
+
+            PopulateSelected();
+            return View(entry);
+        }
+
+        
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+           
+            Entry entry = _entriesRepository.GetEntry((int)id);
+            PopulateSelected();
+            return View(entry);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Entry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                _entriesRepository.UpdateEntry(entry);
+
+                return RedirectToAction("Index");
+            }
+
+            PopulateSelected();
+            return View(entry);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Entry entry = _entriesRepository.GetEntry((int)id);
+
+
+            return View(entry);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            _entriesRepository.DeleteEntry(id);
+
+            return RedirectToAction("Index");
+        }
+
+        private void PopulateSelected()
+        {
+            ViewBag.SelectListItem = new SelectList(Data.Data.Activities, "Id", "Name");
+        }
+
+    }
+}
